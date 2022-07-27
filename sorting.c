b6da82a4b534831cf67da87c2b1fb9f4cb09c595
Amisha Raje <rajeamisha30@gmail.com>
2022-07-27 19:36:06 +0530

    6th commit,insertion sort

---
+++ sorting.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+//printing array after sorting
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+//swapping 2 elements
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+    //insertion sort
+    void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++)
+     {
+      // comparing element of array with key element
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+
+}
+
+//main function
+int main()
+ {
+  
+  int n;
+   printf("choose size of array:\n");
+  scanf("%d", &n);
+ 
+  int data[n];
+  for(int i=0;i<n;i++){
+    printf("enter the %dth element of data:\n", i);
+    scanf("%d", &data[i]);
+  }
+  //various wys for sorting 
+  int op;
+  
+  printf("1. insertion sort\n");
+  /*printf("2. Selection sort\n");
+  printf("3. Bubble sort\n");
+  printf("4. Merge sort\n");
+  printf("5. Quick sort\n");*/
+  printf("input type of sorting to do  :\n");
+  scanf("%d", &op);
+  switch(op)
+  {
+    // sorting of array through insertion sort
+    case 1:
+    insertionSort(data,n);
+    printf("Sorted array in ascending order:\n");
+    printArray(data, n);
+    break;
+ 
+  default:
+  printf("\n You have entered an Invalid input ");
+
+  return 0;
+  }
+ }
